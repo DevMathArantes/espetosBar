427a3522f938952de3ebcdc08248da79da745b30
math.arantes <matheusoficial1411@gmail.com>
2024-11-21 13:57:01 -0300

    atualização script

--- script.js
+++ script.js
@@ -15,27 +15,44 @@ let totalChopp;
 let nomeChopp;
 let linkChopp ="https://wa.me/5516992467515/?text=";
 
-//coleta ids 
-function get(id){
-    return document.getElementById(id);
-}
+//Variável que confirma a montagem da lista
+let montar = true;
 
-//atualiza o Status de 1 para aberto e 0 para fechado 
-function aberto(){
-    const agora = new Date();
-    const hora = agora.getHours();
-    let status = get('status');
-    if((hora>=18) && (hora<=23)){
-        status.innerHTML=`Aberto`;
-        Status = 1;
-    }
-    else{
-        status.innerHTML=`Fechado`;
-        Status = 0;
-    }
-}
+//Variável que recebe a forma de pagamento
+let forma;
 
-//funções auxiliares de adicionar ou retirar itens
+//Funções coringa
+
+    //coleta ids 
+    function get(id){
+        return document.getElementById(id);
+    }
+ 
+//Funções auxiliares
+
+    //adiciona os itens
+    function adicionarItem(identificador){
+        let itens = parseInt(get('quantidade'+identificador).innerHTML);
+        itens++;
+        get('quantidade'+identificador).innerHTML=itens;
+        if(identificador<=38){
+            Total+=valorItem(identificador);
+            verificaTotal();
+        }
+    }
+
+    //remove os itens
+    function retirarItem(identificador){
+        if(parseInt(get('quantidade'+identificador).innerHTML) != 0){
+            let itens = parseInt(get('quantidade'+identificador).innerHTML);
+            itens--;
+            get('quantidade'+identificador).innerHTML=itens;
+            if(identificador<=38){
+                Total-=valorItem(identificador);
+                verificaTotal();
+            }
+        }
+    }
 
     //verifica se o finalizar deve aparecer
     function verificaTotal(){
@@ -48,112 +65,147 @@ function aberto(){
         }
     }
 
-    //retorna o valor do produto como float
+    //retorna o preço do produto como float
     function valorItem(identificador){
         let valor = get('valor'+identificador).innerHTML;
         let valorFinal = parseFloat(valor.slice(3));
         return valorFinal;
     }
 
-//adiciona os itens
-function adicionarItem(identificador){
-    let itens = parseInt(get('quantidade'+identificador).innerHTML);
-    itens++;
-    get('quantidade'+identificador).innerHTML=itens;
-    Total+=valorItem(identificador);
-    verificaTotal();
-}
-
-//remove os itens
-function retirarItem(identificador){
-    if(parseInt(get('quantidade'+identificador).innerHTML) != 0){
-        let itens = parseInt(get('quantidade'+identificador).innerHTML);
-        itens--;
-        get('quantidade'+identificador).innerHTML=itens;
-        Total-=valorItem(identificador);
-        verificaTotal();
-    }
-}
-
-//abre o modal
-function abrirModal(){
-    get('modal').style.display='flex';
-    get('fundoModal').style.display='block';
-}
-
-//fecha o modal
-function fecharModal(){
-    get('modal').style.display='none';
-    get('fundoModal').style.display='none';
-}
-
-//gera a lista final (após as verificações)
-function gerarLista(){
-    if((get('nome').value!="")){
-        link +=get('nome').value+"%0A%0A";
-        if(get('formaPagamento').value!=1 && verificaTroco()){
+    //Monta a lista e o link final, tipo 1 para cardápio e tipo 2 para chopps
+    function coletarPedido(tipo){
+        if(tipo == 1){
             for(let i=1; i<=38;i++){
                 if(parseInt(get('quantidade'+i).innerHTML)> 0){
                     listaFinal+=get('quantidade'+i).innerHTML+" - "+ get('produto'+i).innerHTML+"<br>";
-                    link +=get('quantidade'+i).innerHTML+"%20-%20"+ get('produto'+i).innerHTML+"%20(R$"+(valorItem(i)*get('quantidade'+i).innerHTML).toFixed(2)+")%0A";
+                    link +=get('quantidade'+i).innerHTML
+                    +"%20-%20"
+                    + get('produto'+i).innerHTML+"%20(R$"
+                    +(valorItem(i)*get('quantidade'+i).innerHTML).toFixed(2)
+                    +")%0A";
                 }
             }
-            if(get('campoEntrega').value != ""){
-                listaFinal+="<br>Endereço: "+get('campoEntrega').value+"<br>";
-                link+="%0A%0AEndereço:%20"+get('campoEntrega').value+"%0A";
+        }
+        if(tipo ==2){
+            for(let i=39; i<=42;i++){
+                if(parseInt(get('quantidade'+i).innerHTML)> 0){
+                    
+                }
             }
+        }
+    }
+
+    //Verifica se o campo nome está preenchido
+    function verificaNome(nome){
+        if(get(nome).value == ""){
+            montar = false;
+            alert("Preencha o campo Nome");
+        }
+    }
+
+    //Retorna o valor do troco
+    function calculaTroco(){
+        let dinheiro = parseFloat(get('troco').value);
+        let troco = dinheiro - Total;
+        return (troco.toFixed(2)).toString();
+    }
+
+    //Verifica a forma de pagamento
+    function verificaPagamento(){
+        let tipo = get('formaPagamento').value;
+        if(tipo==1){
+            montar = false;
+            alert("Informe o método de pagamento");
+        }
+        if(tipo==2){
+            forma = "Dinheiro";
+            if((get('troco').value < Total) || (get('troco').value == "")){
+                montar = false;
+                alert("Troco inválido");
+            }
+            else{
+                link+="%0ATroco%20para%20R$"+get('troco').value+"%20(R$%20"+calculaTroco()+")%0A";
+                listaFinal+=`Troco para R$ `+get('troco').value+" (R$ "+calculaTroco()+")<br>";
+            }
+        }
+        if(tipo==3){
+            forma = "Cartão";
+        }
+        if(tipo==4){
+            forma = "Pix";
+        }
+    }
+
+    //Verifica o endereço
+    function verificaEndereço(){
+        let endereco = get('campoEntrega').value;
+        if(endereco != ""){
+            let entrega = get('campoEntrega').value;
+            listaFinal+=`<br>Entregar: `+entrega+`<br>`;
+            link+="%0AEntregar:%20"+entrega+"%0A";
+        }
+    }
+
+//Funções principais
+
+    //atualiza o Status de 1 para aberto e 0 para fechado 
+    function aberto(){
+        const agora = new Date();
+        const hora = agora.getHours();
+        let status = get('status');
+        if((hora>=18) && (hora<=23)){
+            status.innerHTML=`Aberto`;
+            Status = 1;
+        }
+        else{
+            status.innerHTML=`Fechado`;
+            Status = 0;
+        }
+    }
+
+    //abre o modal final
+    function abrirModal(){
+        get('modal').style.display='flex';
+    }
+
+    //fecha o modal final
+    function fecharModal(){
+        get('modal').style.display='none';
+    }
+    
+    //abre o campo para troco
+    function troco(){
+        if(get('formaPagamento').value=="2"){
+            get('troco').style.display='block';
+        }
+        else{
+            get('troco').style.display='none';
+        }
+    }
+
+    //gera a lista final (após as verificações)
+    function gerarLista(){
+        verificaNome('nome');
+        verificaPagamento();
+        verificaEndereço();
+        if(montar){
+            coletarPedido(1);
+            link +=get('nome').value+"%0APago%20em"+forma+"%0A";
+            listaFinal += forma+`<br>`;
             listaFinal+=`<br>Total: R$ ${Total.toFixed(2)}<br>`
             link+="%0ATotal=%20R$"+Total.toFixed(2);
             get('modal').innerHTML+=`<p>${listaFinal}</p>`;
             get('modal').innerHTML+=`<a class="enviarPedido btnPedido" href="${link}">Enviar pedido</a>`
             get('infoCliente').style.display='none';
         }
-        else{
-            alert("Pagamento inválido")
-        }
     }
-    else{
-        alert("Preencha seu nome")
-    }
-}
 
-//abre o campo para troco
-function troco(){
-    if(get('formaPagamento').value=="2"){
-        get('troco').style.display='block';
-    }
-    else{
-        get('troco').style.display='none';
-    }
-}
 
-//Verifica o valor do troco
-function verificaTroco(){
-    if(get('formaPagamento').value=="1"){
-        return false;
-    }
-    else{
-        if(get('formaPagamento').value=="2" && get('troco').value>=Total){
-            let Tr = parseFloat(get('troco').value)-Total;
-            link+="%0ATroco%20para%20R$%20"+parseFloat(get('troco').value).toFixed(2)+"%20-%20(%20R$%20"+parseFloat(Tr).toFixed(2)+")%0A";
-            listaFinal+="<br>Troco para R$ "+parseFloat(get('troco').value).toFixed(2) +" - (R$ "+parseFloat(Tr).toFixed(2)+")<br>";
-            return true;
-        }
-        if(get('formaPagamento').value=="3"){
-            link+="%0ACartão%0A"
-            listaFinal+="Cartão<br>";
-            return true;
-        }
-        if(get('formaPagamento').value=="4"){
-            link+="%0APix%0A"
-            listaFinal+="Pix<br>";
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-}
+
+
+
+
+
 
 //Gera o link para pedir combos
 function pedirCombo(identificador){
@@ -195,40 +247,4 @@ function pedirCombo(identificador){
     }
 }
 
-//Adiciona chopps
-function addChopp(identificador){
-    let quantChopp = get('quantChopp'+identificador).innerHTML
-    quantChopp++;
-    quantChopp.toString();
-    get('quantChopp'+identificador).innerHTML=quantChopp;
-}
-
-//Retira chopps
-function retChopp(identificador){
-    let quantChopp = get('quantChopp'+identificador).innerHTML;
-    if(quantChopp!=0){
-        quantChopp--;
-        quantChopp.toString();
-        get('quantChopp'+identificador).innerHTML=quantChopp;
-    }
-}
-
-//gerar pedido de chopp
-function gerarChopp(identificador){
-    if(get('quantChopp'+identificador).innerHTML!=0){
-        let nome = prompt("Digite o seu nome: ");
-        if(nome===""){
-            alert("Preencha o campo com seu nome.")
-        }
-        else{
-            totalChopp= valorItem(identificador)*parseInt(get('quantChopp'+identificador).innerHTML);
-            nomeChopp= get('chopp'+identificador).innerHTML;
-            linkChopp+= get('quantChopp'+identificador).innerHTML+"%20"+nomeChopp+"%20(s)%20para%20"+nome;
-            get('gerarChopp'+identificador).style.display="none";
-            get('divChopp'+identificador).innerHTML+=`<a class='enviarPedido btnPedido' href="${linkChopp}">Enviar pedido</a>`;
-        }
-    }
-}
-
 aberto();
-
